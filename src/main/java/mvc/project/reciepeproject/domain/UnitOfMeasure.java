a56b13b24e87b8828cf172f93970fd11ad31f4a4
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-05-06 01:04:35 +0530

    created inter class 1to1 domain objects

---
+++ src/main/java/mvc/project/reciepeproject/domain/UnitOfMeasure.java
@@ -0,0 +1,37 @@
+package mvc.project.reciepeproject.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @OneToOne
+    private Ingredient ingredient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return description;
+    }
+
+    public void setUom(String description) {
+        this.description = description ;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+}
