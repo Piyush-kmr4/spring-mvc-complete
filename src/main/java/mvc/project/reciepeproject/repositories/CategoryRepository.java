e0c59c815b15885bda9c25bf4141ca7b7aa36547
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-05-06 19:59:55 +0530

    spring data jpa query methods demo examples

--- src/main/java/mvc/project/reciepeproject/repositories/CategoryRepository.java
+++ src/main/java/mvc/project/reciepeproject/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package mvc.project.reciepeproject.repositories;
 import mvc.project.reciepeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
