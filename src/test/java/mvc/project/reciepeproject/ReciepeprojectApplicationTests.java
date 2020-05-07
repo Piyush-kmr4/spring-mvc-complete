868f59041710bd80016aee03ec2441b2cdc7643f
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-05-08 00:27:53 +0530

    bootstrap styling done in index page

--- src/test/java/mvc/project/reciepeproject/ReciepeprojectApplicationTests.java
+++ src/test/java/mvc/project/reciepeproject/ReciepeprojectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReciepeprojectApplicationTests {
+class  ReciepeprojectApplicationTests {
 
     @Test
     void contextLoads() {
