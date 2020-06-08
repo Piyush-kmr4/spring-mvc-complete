5a230a7ec65c2cb635b8b5ecf2cdd75c08986344
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-06-08 21:59:25 +0530

    1- RecipeController refactored 2- Dropdown value fetched from ENUM

--- src/main/java/mvc/project/reciepeproject/controllers/RecipeController.java
+++ src/main/java/mvc/project/reciepeproject/controllers/RecipeController.java
@@ -16,8 +16,8 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/show")
+
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         System.out.println("Inside Recipe Controller....with id value="+id);
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
@@ -25,16 +25,16 @@ public class RecipeController {
 
     }
 
-    @GetMapping
-    @RequestMapping("recipe/new")
+
+    @GetMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{id}/update")
+
+    @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeform";
@@ -47,8 +47,8 @@ public class RecipeController {
         return "redirect:/recipe/" + savedCommand.getId() +"/show";
     }
 
-    @GetMapping
-    @RequestMapping ("recipe/{id}/delete")
+
+    @GetMapping ("recipe/{id}/delete")
     public String deleteRecipe(@PathVariable String id){
         log.debug("Deleting id: "+ id);
         recipeService.deleteById(Long.valueOf(id));
