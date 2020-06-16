0be9c21ce7ba139f48fbae45e44cb1a56502a8dc
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-06-16 20:05:29 +0530

    selecting and uploading images using spring mvc

---
+++ src/main/java/mvc/project/reciepeproject/service/ImageServiceImpl.java
@@ -0,0 +1,14 @@
+package mvc.project.reciepeproject.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService{
+    @Override
+    public void saveImage(Long recipeId, MultipartFile file) {
+        log.debug("Received a file..");
+    }
+}
